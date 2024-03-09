cb0fd53c43ba4bb7ed37037df58e6d3cb821a718
Chaemin-Lim <antraxmin@naver.com>
2024-03-09 16:00:50 +0900

    Update Layout.tsx

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,15 +1,48 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import Footer from "./Footer";
+import Drawer from "./Drawer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerOpen(false);
+  };
   return (
     <div className="bg-[#F2EFE8] min-h-screen">
       <div className="h-full max-w-md mx-auto ">
-        <div>{children}</div>
+        <div className="h-screen flex flex-col">
+          <div className="flex items-center justify-between p-4">
+            <div>
+              <FontAwesomeIcon
+                icon={faBars}
+                size="lg"
+                onClick={toggleDrawer}
+                className="cursor-pointer"
+              />
+            </div>
+            <div>2024 동아리 박람회</div>
+          </div>
+          <Drawer isOpen={isDrawerOpen} onClose={closeDrawer} />
+          {isDrawerOpen && (
+            <div
+              className="fixed top-0 left-0 h-screen w-full bg-black opacity-50 z-40"
+              onClick={closeDrawer}
+            />
+          )}
+          <div>{children}</div>
+          <Footer />
+        </div>
       </div>
     </div>
   );
