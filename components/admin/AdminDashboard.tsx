2cbb826e7810695f8098986e82b334652ac7f68f
Chaemin-Lim <antraxmin@naver.com>
2024-03-10 17:43:24 +0900

    fix: quill ssr error

--- components/admin/AdminDashboard.tsx
+++ components/admin/AdminDashboard.tsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
-import ReactQuill from "react-quill";
+import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 
 interface AdminDashboardProps {
   categories: string[];
 }
 
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+
 const AdminDashboard: React.FC<AdminDashboardProps> = ({ categories }) => {
   const [content, setContent] = useState<string>("");
 
-  const handleAddNotice = () => {
-    // content 상태를 사용하여 공지사항을 추가하는 로직을 작성하세요.
-  };
+  const handleAddNotice = () => {};
 
   return (
     <div className="flex">
-      {/* Sidebar */}
       <div className="w-1/6 bg-[#2B9F70] h-screen text-white p-4 text-center">
         <h2 className="text-3xl font-bold mt-24 mb-10">ADMIN</h2>
         <ul className="space-y-6">
@@ -26,10 +25,8 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({ categories }) => {
           ))}
         </ul>
       </div>
-      {/* Main content */}
       <div className="w-5/6 p-4">
         <h1 className="text-3xl font-bold mb-4">공지사항 관리</h1>
-        {/* 공지사항 추가 */}
         <div className="mb-6">
           <ReactQuill
             theme="snow"
@@ -43,8 +40,6 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({ categories }) => {
             추가
           </button>
         </div>
-        {/* 공지사항 목록 */}
-        {/* 필요한 공지사항 목록 UI를 추가하세요. */}
       </div>
     </div>
   );
